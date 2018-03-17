5687beb9846131666cf793db5c6c43fe43479341
pos0637 <pos0637@163.com>
2018-03-17 17:52:18 +0800

    添加注释。

--- sources/server/src/main/java/com/furongsoft/ems/controllers/api/v1/SystemController.java
+++ sources/server/src/main/java/com/furongsoft/ems/controllers/api/v1/SystemController.java
@@ -28,6 +28,7 @@ import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 系统控制器
@@ -48,6 +49,16 @@ public class SystemController {
         this.permissionService = permissionService;
     }
 
+    /**
+     * 获取全部资源
+     *
+     * @param pageRequest 页面
+     * @param name        名称
+     * @param path        路径
+     * @param sortField   排序字段
+     * @param sortType    排序方式
+     * @return 全部资源
+     */
     @GetMapping("/resources")
     public PageResponse getResources(
             PageRequest pageRequest,
@@ -59,17 +70,36 @@ public class SystemController {
         return new PageResponse<>(HttpStatus.OK, page);
     }
 
+    /**
+     * 获取资源
+     *
+     * @param id 资源索引
+     * @return 响应内容
+     */
     @GetMapping("/resources/{id}")
     public RestResponse getResource(@NonNull @PathVariable String id) {
         return new RestResponse(HttpStatus.OK, null, resourceService.get(id));
     }
 
+    /**
+     * 添加资源
+     *
+     * @param resource 资源
+     * @return 响应内容
+     */
     @PostMapping("/resources")
     public RestResponse addResource(@NonNull @RequestParam Resource resource) {
         resourceService.add(resource);
         return new RestResponse(HttpStatus.OK);
     }
 
+    /**
+     * 更新资源
+     *
+     * @param id       资源索引
+     * @param resource 资源
+     * @return 响应内容
+     */
     @PutMapping("/resources/{id}")
     public RestResponse editResource(@NonNull @PathVariable String id, @NonNull @RequestParam Resource resource) {
         resource.setId(id);
@@ -77,12 +107,24 @@ public class SystemController {
         return new RestResponse(HttpStatus.OK);
     }
 
+    /**
+     * 删除资源
+     *
+     * @param id 资源索引
+     * @return 响应内容
+     */
     @DeleteMapping("/resources/{id}")
     public RestResponse delResource(@NonNull @PathVariable String id) {
         resourceService.del(id);
         return new RestResponse(HttpStatus.OK);
     }
 
+    /**
+     * 批量删除资源
+     *
+     * @param delete 资源索引列表
+     * @return 响应内容
+     */
     @PostMapping("/resources/batch")
     public RestResponse delResources(@NonNull @RequestParam String delete) {
         try {
@@ -96,9 +138,27 @@ public class SystemController {
         return new RestResponse(HttpStatus.OK);
     }
 
+    /**
+     * 获取权限列表
+     *
+     * @return 权限列表
+     */
     @GetMapping("/permissions")
     public RestResponse getPermissions() {
         TreeNode<Permission> root = permissionService.getPermissionsTree(permissionService.getPermissions());
+        return new RestResponse(HttpStatus.OK, null, JSON.toJSONString(root.children));
+    }
+
+    /**
+     * 获取菜单列表
+     *
+     * @return 菜单列表
+     */
+    @GetMapping("/menus")
+    public RestResponse getMenus() {
+        List<Permission> list = permissionService.getPermissions();
+        list = list.stream().filter(permission -> (permission.getType() == 0)).collect(Collectors.toList());
+        TreeNode<Permission> root = permissionService.getPermissionsTree(list);
         return new RestResponse(HttpStatus.OK, null, JSON.toJSONString(root.children));
     }
 
