b6558faa001bd1fc02ec11269c620eee2bacc3f8
pos0637 <pos0637@163.com>
2018-03-24 15:12:45 +0800

    添加公司信息前端显示。

--- sources/server/src/main/java/com/furongsoft/ems/controllers/cms/CmsHomeController.java
+++ sources/server/src/main/java/com/furongsoft/ems/controllers/cms/CmsHomeController.java
@@ -1,8 +1,9 @@
 package com.furongsoft.ems.controllers.cms;
 
+import com.furongsoft.ems.services.cms.CompanyInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -14,9 +15,16 @@ import java.util.Map;
 @Controller
 @RequestMapping("/cms/home")
 public class CmsHomeController {
+    private final CompanyInfoService companyInfoService;
+
+    @Autowired
+    public CmsHomeController(CompanyInfoService companyInfoService) {
+        this.companyInfoService = companyInfoService;
+    }
+
     @RequestMapping("/index")
     public String index(Map<String, Object> map) {
-        map.put("hello", "from TemplateController.helloHtml");
+        map.put("companyInfo", companyInfoService.get(null).getInformation());
         return "resources/cms/templates/template1/views/homepage/index.html";
     }
 }
